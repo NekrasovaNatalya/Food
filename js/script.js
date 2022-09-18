b54679d283dd94f688303fa4d1fa9aef89b8df0a
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-18 12:00:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -310,4 +310,24 @@ window.addEventListener('DOMContentLoaded', () => {
         slidesWrapper = document.querySelector('.offer__slider-wrapper'),
         width = window.getComputedStyle(slidesWrapper).width,
         slidesField = document.querySelector('.offer__slider-inner');
+    
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
 });
